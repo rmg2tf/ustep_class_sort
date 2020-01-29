54d0e9c528ea969f6fc7e3517bdd9979b287b710
rmg2tf <60373466+rmg2tf@users.noreply.github.com>
2020-01-29 15:12:14 -0500

    Update ustep_class_sort.m

--- ustep_class_sort.m
+++ ustep_class_sort.m
@@ -32,12 +32,40 @@ form = readtable(filename);
 firstname = form(:,'FirstName');
 lastname = form(:,'LastName');
 
-kid = [firstname,lastname];
+% c1c1 = class 1 choice 1 etc adjust for # of classes
 
+c1c1 = form(:, 'Class1_RankTheClassesInOrderOfPreference_1stChoice_');
+c1c2 = form(:, 'Class1_RankTheClassesInOrderOfPreference_2ndChoice_');
+c1c3 = form(:, 'Class1_RankTheClassesInOrderOfPreference_3rdChoice_');
+c1c4 = form(:, 'Class1_RankTheClassesInOrderOfPreference_4thChoice_');
+c1c5 = form(:, 'Class1_RankTheClassesInOrderOfPreference_5thChoice_');
+
+c2c1 = form(:, 'Class2_RankTheClassesInOrderOfPreference_1stChoice_');
+c2c2 = form(:, 'Class2_RankTheClassesInOrderOfPreference_2ndChoice_');
+c2c3 = form(:, 'Class2_RankTheClassesInOrderOfPreference_3rdChoice_');
+c2c4 = form(:, 'Class2_RankTheClassesInOrderOfPreference_4thChoice_');
+c2c5 = form(:, 'Class2_RankTheClassesInOrderOfPreference_5thChoice_');
+
+c3c1 = form(:, 'Class3_RankTheClassesInOrderOfPreference_1stChoice_');
+c3c2 = form(:, 'Class3_RankTheClassesInOrderOfPreference_2ndChoice_');
+c3c3 = form(:, 'Class3_RankTheClassesInOrderOfPreference_3rdChoice_');
+c3c4 = form(:, 'Class3_RankTheClassesInOrderOfPreference_4thChoice_');
+
+c4c1 = form(:, 'Class4_RankTheClassesInOrderOfPreference_1stChoice_');
+c4c2 = form(:, 'Class4_RankTheClassesInOrderOfPreference_2ndChoice_');
+c4c3 = form(:, 'Class4_RankTheClassesInOrderOfPreference_3rdChoice_');
+c4c4 = form(:, 'Class4_RankTheClassesInOrderOfPreference_4thChoice_');
+
+kid = [firstname,lastname, c1c1, c1c2, c1c3, c1c4, c1c5, c2c1, c2c2, c2c3, c2c4, c2c5, c3c1, c3c2, c3c3, c3c4, c4c1, c4c2, c4c3, c4c4];
+
+kid = table2array(kid);
 
 % Try next time:
-% convert to array instead table
-% but table nice for readability
+% convert to array instead table 
+% UPDATE: (done line 61)
+% but table nice for readability 
+% UPDATE: (can always convert back)
+
 % try and set up loop for class block 1
 
 
